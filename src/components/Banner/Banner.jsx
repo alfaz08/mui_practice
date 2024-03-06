daa52717d01c9b39afd96d63742bc6ac46e29ff2
Alfaz Hossain <alfaz.cse@gmail.com>
2024-03-06 19:00:50 +0600

    banner is added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Button, Container, Grid, Typography } from "@mui/material";
 
 
 const Banner = () => {
@@ -12,7 +12,26 @@ const Banner = () => {
       py:23
     }}
   >
-    sfdsfsdfsdf
+    <Container sx={{height:'100%'}}>
+    <Grid justifyContent={'start'} alignItems={'center'}>
+    <Grid item xs={12} md={8} >
+      <Typography sx={{color:'white'}} variant="h2">
+          Trusted dental Care
+      </Typography >
+      <Typography gutterBottom   sx={{color:'white'}}  >
+        Lorem ipsum dolor sdffsdf sit amet consectetur adipisicing elit. Quidem el
+      </Typography>
+
+      <Grid sx={{mt:4}}>
+      <Button  size="large" variant="contained" color="primary" >Book Now</Button>
+      <Button size="large" sx={{ml:4}} variant="contained" color="secondary">Buy Now</Button>
+      </Grid>
+
+
+    </Grid>
+    </Grid>
+    
+    </Container>
   </Grid>
   );
 };
